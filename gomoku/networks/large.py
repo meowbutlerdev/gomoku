94e0a604ca5f3e72c44b4503de40d1781acd26be
JISUNG PARK <llllllllll@kakao.com>
2020-08-06 00:15:15 +0900

    feat: Add medium/large Neural Network
    중간/큰 합성곱 신경망 추가

---
+++ gomoku/networks/large.py
@@ -0,0 +1,42 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# Apache License 2.0
+
+from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.convolutional import Conv2D, ZeroPadding2D
+
+# 오목 수 예측용 큰 합성곱 신경망 층 정의
+def layers(input_shape):
+    return [
+        ZeroPadding2D((3, 3), input_shape=input_shape, data_format='channels_first'),
+        Conv2D(64, (7, 7), padding='valid', data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(64, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(64, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(48, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(48, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(32, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        ZeroPadding2D((2, 2), data_format='channels_first'),
+        Conv2D(32, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        Flatten(),
+        Dense(1024),
+        Activation('relu'),
+    ]
