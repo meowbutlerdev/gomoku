31d01546e9137a7c22d16da1f8ce0c77ad31a7be
JISUNG PARK <llllllllll@kakao.com>
2020-08-13 01:21:06 +0900

    feat: Add experience buffer generator
    경험 버퍼 생성자 추가

---
+++ gomoku/rl/experience.py
@@ -0,0 +1,62 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# Apache License 2.0
+
+# 단일 에피소드 상의 결정 추적 클래스
+class ExperienceCollector:
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.current_episode_states = []
+        self.current_episode_actions = []
+
+    def begin_episode(self):
+        self.current_episode_states = []
+        self.current_episode_actions = []
+
+    # 현재 에피소드의 단일 결정을 기록
+    # 에이전트가 상태와 행동을 변환해서 전달
+    def record_decision(self, state, action):
+        self.current_episode_states.append(state)
+        self.current_episode_actions.append(action)
+
+    def complete_episode(self, reward):
+        num_states = len(self.current_episode_states)
+        self.states += self.current_episode_states
+        self.actions += self.current_episode_actions
+        # 최종 보상을 대국의 모든 행동에 전달
+        self.rewards += [reward for _ in range(num_states)]
+
+        self.current_episode_states = []
+        self.current_episode_actions = []
+
+    def to_buffer(self):
+        # list to numpy array
+        return ExperienceBuffer(
+            states=np.array(self.states),
+            actions=np.array(self.actions),
+            rewards=np.array(self.rewards)
+        )
+
+# 경험 버퍼 생성자
+class ExperienceBuffer:
+    def __init__(self, states, actions, rewards):
+        self.states = states
+        self.actions = actions
+        self.rewards = rewards
+
+    # 경험 버퍼를 디스크에 저장
+    def serialize(self, h5file):
+        h5file.create_group('experience')
+        h5file['experience'].create_dataset('states', data=self.states)
+        h5file['experience'].create_dataset('actions', data=self.actions)
+        h5file['experience'].create_dataset('rewards', data=self.rewards)
+
+# HDF5 파일에서 ExperienceBuffer 로드
+def load_experience(h5file):
+    return ExperienceBuffer(
+        states=np.array(h5file['experience']['states']),
+        actions=np.array(h5file['experience']['actions']),
+        rewards=np.array(h5file['experience']['rewards'])
+    )
