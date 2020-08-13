2f2ce5ced586223e2e27c0755e8147dd78d377ac
JISUNG PAKR <llllllllll@kakao.com>
2020-08-13 10:47:33 +0900

    fix: Restore data

---
+++ gomoku/data/__init__.py
@@ -0,0 +1,8 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# Apache License 2.0
+
+from .generator import *
+from .index_processor import *
+from .processor import *
+from .sampling import *
